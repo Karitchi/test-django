47954fd3cd5a551f87dac8eed0b585b47a8ad9ac
Karitchi <karichikarichikarichi@gmail.com>
2023-02-09 19:56:51 +0100

    removed dummy data and use db instead

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 
-posts = [
-    {
-        'title': 'blog post 1',
-        'author': 'coreyms',
-        'date_posted': 'august 27, 2018',
-        'content': 'first post content',
-    },
-    {
-        'title': 'blog post 2',
-        'author': 'jane',
-        'date_posted': 'august 29, 2018',
-        'content': 'second post content',
-    }
-
-]
-
-
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
